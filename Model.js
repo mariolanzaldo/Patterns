2542edff586c9f80547ca7685b50d2fc6554d1a4
mariolanzaldo <100377731+mariolanzaldo@users.noreply.github.com>
2022-06-13 21:08:04 -0500

    Update Model.js

--- Model.js
+++ Model.js
@@ -1,52 +1,55 @@
 export default class Model {
     constructor() {
-        console.log('model')
 
-        this.notes = JSON.parse(localStorage.getItem('stickynotes-notes') || []);
     }
-    
 
-    // bindNotesListChanged(callback){
-    //     this.onNotesListChanged = callback;
-    // }
-
-    // _commit (notes){
-    //     this.bindNotesListChanged(notes);
-    //     localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
-    // }
+    getAllNotes() {
+        const notes = JSON.parse(localStorage.getItem("stickynotes-notes") || "[]");
+        return notes;
+    }
 
     saveNote(noteToSave) {
-        const existing = this.notes.find(note => note.id == noteToSave.id);
-        if (existing && existing.content !== noteToSave.content) {
-            existing.content = noteToSave.content;
-            existing.updated = new Date().toISOString();
-            localStorage.setItem("stickynotes-notes", JSON.stringify(this.notes));
+        const notes = this.getAllNotes();
+        const existing = notes.find(note => note.id == noteToSave.id);
+        if (existing) {
+            if (existing.content !== noteToSave.content && noteToSave.undoUpdate === undefined) {
+                existing.content = noteToSave.content;
+                existing.updated = new Date().toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" });
+            } else if (existing.content !== noteToSave.content && noteToSave.undoUpdate !== undefined) {
+                existing.content = noteToSave.content;
+                existing.updated = noteToSave.undoUpdate;
+            }
+            localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
         }
     }
 
     addNote(note) {
+        let notes = this.getAllNotes();
         let noteObject = {};
-        if(!note){
+        if (!note) {
             noteObject = {
                 id: Date.now(),
                 content: "",
-                create: `${new Date().toLocaleString()}`
+                create: `${new Date().toLocaleString(undefined, { dateStyle: "full", timeStyle: "short" })}`
             }
-            this.notes.push(noteObject);
-        } 
-        localStorage.setItem("stickynotes-notes", JSON.stringify(this.notes));
-        return noteObject;
+            notes.push(noteObject);
+            localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
+            return noteObject
+        } else if (note) {
+            notes.push(note);
+            localStorage.setItem("stickynotes-notes", JSON.stringify(notes));
+        }
     }
 
     deleteNote(id) {
-        const newNotes = this.notes.filter(note => note.id != id);
-
+        const notes = this.getAllNotes();
+        const newNotes = notes.filter(note => note.id != id);
         localStorage.setItem("stickynotes-notes", JSON.stringify(newNotes));
         return newNotes;
     }
 
     searchNote(noteToSearch) {
-        const found = this.notes.map(element => {
+        const found = this.getAllNotes().map(element => {
             const content = element.content.toLowerCase().includes(noteToSearch);
             if (!content) {
                 return element;
@@ -55,11 +58,11 @@ export default class Model {
         return found;
     }
 
-    dragDrogNote(newOrder) {
+    dragDropNote(newOrder) {
         const oldOrder = [];
         let swappedElement = [];
-        let newData = this.notes;
-        this.notes.forEach(item => {
+        let newData = this.getAllNotes();
+        this.getAllNotes().forEach(item => {
             oldOrder.push(item.id);
         });
 
@@ -75,7 +78,5 @@ export default class Model {
         newData[obj.first] = newData[obj.second];
         newData[obj.second] = temp;
         localStorage.setItem("stickynotes-notes", JSON.stringify(newData));
-
     }
-
 }
